a1ff86b0f932e25207655824c4a159bc5e8899f8
bofeiw <bofei@mail.com>
2019-11-22 02:29:05 +1100

    update repord.pdf

--- complie-submit.sh
+++ complie-submit.sh
@@ -2,8 +2,6 @@ mkdir .temp
 mkdir .temp/server
 mkdir .temp/client
 
-pandoc report.md -s -o report.pdf
-
 cp report.pdf .temp
 cp server/server.py .temp/server
 cp server/UserManager.py .temp/server
@@ -17,5 +15,4 @@ scp assign.tar cse:~/3331
 ssh cse "cd 3331; give cs3331 assign assign.tar"
 
 cd ..
-cp ./.temp/report.pdf .
 rm -rf .temp
